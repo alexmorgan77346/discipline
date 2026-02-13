597e94e18c212bfe952ee5d80d6c61090b100e85
alexmorgan77346 <anshuraj77346@gmail.com>
2026-02-13 21:14:27 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'discipline-dashboard-v4';
+const CACHE_NAME = 'discipline-dashboard-v5';
 const urlsToCache = [
   './',
   './index.html',
@@ -50,6 +50,7 @@ self.addEventListener('activate', event => {
   );
   self.clients.claim();
 });
+
 
 
 
